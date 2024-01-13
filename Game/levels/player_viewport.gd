fb59d719d255a9dbd9ecd5b7e0fa47fac1ec4898
Michael Lugo <michaelrlugo@gmail.com>
2024-01-13 18:50:13 -0500

    Added mph label to viewport.

--- Game/levels/player_viewport.gd
+++ Game/levels/player_viewport.gd
@@ -4,6 +4,7 @@ class_name PlayerViewport
 
 @onready var viewport: SubViewport = $SubViewport
 @onready var camera: CarCamera = $SubViewport/CarCamera
+@onready var mph_Label: Label = $CanvasLayer/Label
 
 var player_info: PlayerInfo = null
 var input: InputProfile
@@ -19,3 +20,6 @@ func _ready():
 		viewport.world_3d = _init_world
 
 	camera.car = player_info.instance
+
+func _process(_delta):
+	mph_Label.text = "MPH: %s" % round(player_info.instance.mph)
