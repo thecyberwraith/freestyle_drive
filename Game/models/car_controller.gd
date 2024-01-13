45b8dd39372bca2ee7f0fc47ddd1b3daa8f62c01
Michael Lugo <michaelrlugo@gmail.com>
2024-01-13 18:49:28 -0500

    Slowed cars down, added mph metric, and slowdowns during turning.

--- Game/models/car_controller.gd
+++ Game/models/car_controller.gd
@@ -4,18 +4,26 @@ class_name PlayerVehicle
 
 var input: InputProfile = InputProfile.new()
 var reset_timer: float = 0.0
-
+var mph: float:
+	get:
+		var rpm: float = ($rear_left.get_rpm() + $rear_right.get_rpm()) / 2
+		return 2 * PI * $rear_left.wheel_radius * rpm * 60 * 3.28084 / 5280
+		
 @export var max_rpm = 400
 @export var max_torque = 1000
 @export var max_tire_angle = 0.6
 
 func _physics_process(delta):
-	steering = lerp(steering, input.get_steering() * (-max_tire_angle), 2 * delta)
+	steering = lerp(steering, input.get_steering() * (-max_tire_angle), 1.5*delta)
 	var acceleration: float = input.get_forward() - input.get_brake()
-	var rpm = $rear_left.get_rpm()
-	$rear_left.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
-	rpm = $rear_right.get_rpm()
-	$rear_right.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
+	
+	_set_wheel_force($rear_left, acceleration)
+	_set_wheel_force($rear_right, acceleration)
+
+func _set_wheel_force(wheel: VehicleWheel3D, acceleration: float):
+	var rpm: float = wheel.get_rpm()
+	var damper: float = (1-(rpm/max_rpm)) * (1-(abs(steering)/max_tire_angle))
+	wheel.engine_force = acceleration * max_torque * damper
 
 func _process(delta):
 	var rel_up: Vector3 = transform.basis.y
